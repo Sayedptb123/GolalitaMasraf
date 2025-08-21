4bc9eac3700d62614fd478240bb57b5950797a6c
Sayedptb123 <sayedptb@gmail.com>
2025-08-21 13:48:10 +0530

    ios directory updated

--- ios/GolalitaMasraf/AppDelegate.swift
+++ ios/GolalitaMasraf/AppDelegate.swift
@@ -2,9 +2,14 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import GoogleMaps
+import Firebase
+import UserNotifications
+import RNCPushNotificationIOS
+import CodePush
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate {
   var window: UIWindow?
 
   var reactNativeDelegate: ReactNativeDelegate?
@@ -14,6 +19,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    
+    // Configure Google Maps
+    GMSServices.provideAPIKey("AIzaSyAYjhzanBtI_zeCxlnel5cw7zriaWTcOTo")
+    
+    // Configure Firebase
+    FirebaseApp.configure()
+    
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
@@ -24,12 +36,76 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "GolalitaMasraf",
+      withModuleName: "GolalitaMasraf", // Matches app.json name
       in: window,
       launchOptions: launchOptions
     )
+    
+    // Configure Push Notifications
+    UNUserNotificationCenter.current().delegate = self
+    
+    let center = UNUserNotificationCenter.current()
+    let authOptions: UNAuthorizationOptions = [.alert, .sound, .badge]
+    center.requestAuthorization(options: authOptions) { granted, error in
+      // Handle authorization result
+    }
+    
+    // Register for remote notifications
+    application.registerForRemoteNotifications()
 
     return true
+  }
+  
+  // MARK: - Push Notification Methods
+  
+  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    let fcmToken = Messaging.messaging().fcmToken
+    print("FCM registration token: \(fcmToken ?? "nil")")
+    
+    RNCPushNotificationIOS.didRegisterForRemoteNotifications(withDeviceToken: deviceToken)
+  }
+  
+  func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    RNCPushNotificationIOS.didFailToRegisterForRemoteNotifications(withError: error)
+  }
+  
+  func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    RNCPushNotificationIOS.didReceiveRemoteNotification(userInfo, fetchCompletionHandler: completionHandler)
+  }
+  
+  // MARK: - UNUserNotificationCenterDelegate
+  
+  func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    RNCPushNotificationIOS.didReceiveNotificationResponse(response)
+    completionHandler()
+  }
+  
+  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    let userInfo = notification.request.content.userInfo
+    print("APP_PUSH from foreground \(userInfo)")
+    
+    RNCPushNotificationIOS.didReceiveRemoteNotification(userInfo) { _ in }
+    completionHandler([.sound, .alert, .badge])
+  }
+  
+  // MARK: - Deep Linking
+  
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+    return RCTLinkingManager.application(app, open: url, options: options)
+  }
+  
+  // MARK: - RCTBridgeDelegate
+  
+  func sourceURL(for bridge: RCTBridge!) -> URL! {
+    return self.sourceURL()
+  }
+  
+  func sourceURL() -> URL! {
+#if DEBUG
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    return CodePush.bundleURL()
+#endif
   }
 }
 
@@ -40,9 +116,9 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
 
   override func bundleURL() -> URL? {
 #if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
 #else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    return CodePush.bundleURL()
 #endif
   }
 }
