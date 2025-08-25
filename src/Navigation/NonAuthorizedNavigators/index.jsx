1b8c979dffd007955865e7ac24acd2e6b31aad48
Sayedptb123 <sayedptb@gmail.com>
2025-08-25 15:59:54 +0530

    navigation in progress

---
+++ src/Navigation/NonAuthorizedNavigators/index.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useDispatch } from 'react-redux';
+import { setIsSplashScreenVisible } from '../../redux/auth/auth-actions';
+import Login from '../../AuthScreens/Login';
+import OnBoarding from '../../AuthScreens/OnBoarding/OnBoarding';
+import ForgotPassword from '../../AuthScreens/ForgotPassword';
+import Register from '../../AuthScreens/Register/Register';
+import RegCodeVerification from '../../AuthScreens/Register/RegCodeVerification';
+import CreatePassword from '../../AuthScreens/CreatePassword/CreatePassword';
+import Verification from '../../AuthScreens/Verification/Verification';
+import PrivacyPolicy from '../../MainScreens/PrivacyPolicy';
+
+const Stack = createStackNavigator();
+
+export const NonUthorizedNavigators = () => {
+  const [isOnBoarding, setIsOnBoarding] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      setTimeout(() => {
+        dispatch(setIsSplashScreenVisible(false));
+      }, 3000);
+
+      const isBoard = await AsyncStorage.getItem('isBoard');
+      setIsOnBoarding(JSON.parse(isBoard) === null);
+    })();
+  }, []);
+  if (isOnBoarding === null) return null;
+  return (
+    <Stack.Navigator
+      initialRouteName={isOnBoarding ? 'OnBoarding' : 'Login'}
+      screenOptions={{
+        headerShown: false,
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+      }}
+    >
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="OnBoarding" component={OnBoarding} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen
+        name="RegCodeVerification"
+        component={RegCodeVerification}
+      />
+      <Stack.Screen name="CreatePassword" component={CreatePassword} />
+      <Stack.Screen name="Verification" component={Verification} />
+      <Stack.Screen name={'PrivacyPolicy'} component={PrivacyPolicy} />
+    </Stack.Navigator>
+  );
+};
