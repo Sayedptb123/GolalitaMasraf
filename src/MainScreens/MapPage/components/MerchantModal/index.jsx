da3cf74eb3ef90ead080a094dcb1116432bcac7c
Sayedptb123 <sayedptb@gmail.com>
2025-09-10 14:12:58 +0530

    code clenaup

--- src/MainScreens/MapPage/components/MerchantModal/index.jsx
+++ src/MainScreens/MapPage/components/MerchantModal/index.jsx
@@ -1,30 +1,31 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { ActivityIndicator, Linking, StyleSheet, View } from "react-native";
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { ActivityIndicator, Linking, StyleSheet, View } from 'react-native';
 import {
   BottomSheetModal,
   BottomSheetModalProvider,
-} from "@gorhom/bottom-sheet";
-import Portal from "../../../../components/Portal";
-import { BALOO_MEDIUM } from "../../../../redux/types";
-import { useTheme } from "../../../../components/ThemeProvider";
-import { TouchableOpacity } from "react-native";
-import { TypographyText } from "../../../../components/Typography";
-import { colors } from "../../../../components/colors";
-import { Image } from "react-native";
-import store from "../../../../redux/store";
-import { getMerchantDetails } from "../../../../redux/merchant/merchant-thunks";
-import { getCategoryNameByIdAndLang } from "../../../../components/Categories/helpres";
-import { mainStyles } from "../../../../styles/mainStyles";
-import { isRTL } from "../../../../../utils";
-import { useTranslation } from "react-i18next";
-import Distance from "./components/Distance";
-import { getMerchantDisscountForOffers } from "../../../../api/merchants";
+  BottomSheetView,
+} from '@gorhom/bottom-sheet';
+import Portal from '../../../../components/Portal';
+import { BALOO_MEDIUM } from '../../../../redux/types';
+import { useTheme } from '../../../../components/ThemeProvider';
+import { TouchableOpacity } from 'react-native';
+import { TypographyText } from '../../../../components/Typography';
+import { colors } from '../../../../components/colors';
+import { Image } from 'react-native';
+import store from '../../../../redux/store';
+import { getMerchantDetails } from '../../../../redux/merchant/merchant-thunks';
+import { getCategoryNameByIdAndLang } from '../../../../components/Categories/helpres';
+import { mainStyles } from '../../../../styles/mainStyles';
+import { isRTL } from '../../../../../utils';
+import { useTranslation } from 'react-i18next';
+import Distance from './components/Distance';
+import { getMerchantDisscountForOffers } from '../../../../api/merchants';
 import {
   getCurrentLocation,
   requestLocationPermission,
 } from '../../../../helpers';
 
-const snapPoints = ["30%"];
+const snapPoints = ['30%'];
 
 const MerchantModal = ({ merchant, setSelectedMerchant }) => {
   const { isDark } = useTheme();
@@ -41,7 +42,7 @@ const MerchantModal = ({ merchant, setSelectedMerchant }) => {
     bottomSheetModalRef.current?.close();
   }, []);
 
-  const handleSheetChanges = useCallback((index) => {
+  const handleSheetChanges = useCallback(index => {
     if (index) {
       setSelectedMerchant(null);
     }
@@ -52,12 +53,12 @@ const MerchantModal = ({ merchant, setSelectedMerchant }) => {
       setLoadingDescription(true);
 
       const description = await getMerchantDisscountForOffers(
-        merchant.merchant_id
+        merchant.merchant_id,
       );
 
       setDescription(description);
     } catch (err) {
-      console.log(err, "err");
+      console.log(err, 'err');
     } finally {
       setLoadingDescription(false);
     }
@@ -83,6 +84,7 @@ const MerchantModal = ({ merchant, setSelectedMerchant }) => {
 
   const handleNavigatePress = async () => {
     bottomSheetModalRef.current?.close();
+
     try {
       const status = await requestLocationPermission();
 
@@ -100,10 +102,11 @@ const MerchantModal = ({ merchant, setSelectedMerchant }) => {
           return;
         }
       }
-    Linking.openURL(
-      `https://www.google.com/maps/dir/Current+Location/${merchant.partner_latitude},${merchant.partner_longitude}`
-    ); 
-  } catch (err) {
+
+      Linking.openURL(
+        `https://www.google.com/maps/dir/Current+Location/${merchant.partner_latitude},${merchant.partner_longitude}`,
+      );
+    } catch (err) {
       console.log(err, 'err');
     }
   };
@@ -124,11 +127,11 @@ const MerchantModal = ({ merchant, setSelectedMerchant }) => {
   const backgroundColor = isDark ? colors.darkBlue : colors.white;
 
   return (
-    <Portal name={"merchant-map-modal"}>
-      <BottomSheetModalProvider key={"merchant-map-modal"}>
+    <Portal name={'merchant-map-modal'}>
+      <BottomSheetModalProvider key={'merchant-map-modal'}>
         <View style={styles.container}>
           <BottomSheetModal
-            key={"merchant-map-modal"}
+            key={'merchant-map-modal'}
             ref={bottomSheetModalRef}
             snapPoints={snapPoints}
             onChange={handleSheetChanges}
@@ -136,100 +139,106 @@ const MerchantModal = ({ merchant, setSelectedMerchant }) => {
               backgroundColor,
             }}
           >
-            <TouchableOpacity
-              onPress={handleDetailsPress}
-              style={[
-                styles.wrapper,
-                {
-                  backgroundColor,
-                },
-              ]}
-            >
-              <View>
-                <View style={styles.cardWrapper}>
-                  <Image
-                    source={{ uri: merchant?.merchant_logo }}
-                    style={[
-                      styles.logo,
-                      {
-                        borderColor: isDark
-                          ? colors.mainDarkMode
-                          : colors.darkBlue,
-                        borderWidth: 2,
-                      },
-                    ]}
-                  />
-
-                  <View style={styles.textBlock}>
-                    <TypographyText
-                      title={
-                        isRTL()
-                          ? merchant?.x_arabic_name
-                          : merchant?.merchant_name
-                      }
-                      textColor={isDark ? colors.mainDarkMode : colors.darkBlue}
-                      size={16}
-                      font={BALOO_MEDIUM}
-                      numberOfLines={2}
-                      style={styles.name}
+            <BottomSheetView>
+              <TouchableOpacity
+                onPress={handleDetailsPress}
+                style={[
+                  styles.wrapper,
+                  {
+                    backgroundColor,
+                  },
+                ]}
+              >
+                <View>
+                  <View style={styles.cardWrapper}>
+                    <Image
+                      source={{ uri: merchant?.merchant_logo }}
+                      style={[
+                        styles.logo,
+                        {
+                          borderColor: isDark
+                            ? colors.mainDarkMode
+                            : colors.darkBlue,
+                          borderWidth: 2,
+                        },
+                      ]}
                     />
 
-                    <TypographyText
-                      title={
-                        isRTL()
-                          ? description?.x_ribbon_text_arabic || ""
-                          : description?.ribbon_text || ""
-                      }
-                      textColor={isDark ? colors.mainDarkMode : colors.darkBlue}
-                      size={16}
-                      font={BALOO_MEDIUM}
-                      numberOfLines={2}
-                      style={styles.name}
-                    />
-
-                    {loadingDescription && <ActivityIndicator />}
-
-                    <View style={styles.navigateBtn}>
-                      <Distance
-                        latitude={merchant?.partner_latitude}
-                        longitude={merchant?.partner_longitude}
+                    <View style={styles.textBlock}>
+                      <TypographyText
+                        title={
+                          isRTL()
+                            ? merchant?.x_arabic_name
+                            : merchant?.merchant_name
+                        }
+                        textColor={
+                          isDark ? colors.mainDarkMode : colors.darkBlue
+                        }
+                        size={16}
+                        font={BALOO_MEDIUM}
+                        numberOfLines={2}
+                        style={styles.name}
                       />
 
-                      <TouchableOpacity
-                        onPress={handleNavigatePress}
-                        style={[
-                          styles.btn,
-                          {
-                            borderColor,
-                          },
-                        ]}
-                      >
-                        <TypographyText
-                          title={t("MapPage.navigate")}
-                          textColor={
-                            isDark ? colors.mainDarkMode : colors.darkBlue
-                          }
-                          size={14}
-                          font={BALOO_MEDIUM}
-                          numberOfLines={1}
+                      <TypographyText
+                        title={
+                          isRTL()
+                            ? description?.x_ribbon_text_arabic || ''
+                            : description?.ribbon_text || ''
+                        }
+                        textColor={
+                          isDark ? colors.mainDarkMode : colors.darkBlue
+                        }
+                        size={16}
+                        font={BALOO_MEDIUM}
+                        numberOfLines={2}
+                        style={styles.name}
+                      />
+
+                      {loadingDescription && <ActivityIndicator />}
+
+                      <View style={styles.navigateBtn}>
+                        <Distance
+                          latitude={merchant?.partner_latitude}
+                          longitude={merchant?.partner_longitude}
                         />
-                      </TouchableOpacity>
+                        <TouchableOpacity
+                          onPress={handleNavigatePress}
+                          style={[
+                            styles.btn,
+                            {
+                              borderColor,
+                            },
+                          ]}
+                        >
+                          <TypographyText
+                            title={t('MapPage.navigate')}
+                            textColor={
+                              isDark ? colors.mainDarkMode : colors.darkBlue
+                            }
+                            size={14}
+                            numberOfLines={1}
+                          />
+                        </TouchableOpacity>
+                      </View>
                     </View>
+                    <TouchableOpacity
+                      onPress={onClosePress}
+                      style={styles.closeWrapper}
+                    >
+                      <TypographyText
+                        title={'X'}
+                        textColor={
+                          isDark ? colors.mainDarkMode : colors.darkBlue
+                        }
+                        size={16}
+                        font={BALOO_MEDIUM}
+                      />
+                    </TouchableOpacity>
                   </View>
-                  <TouchableOpacity
-                    onPress={onClosePress}
-                    style={styles.closeWrapper}
-                  >
-                    <TypographyText
-                      title={"X"}
-                      textColor={isDark ? colors.mainDarkMode : colors.darkBlue}
-                      size={16}
-                      font={BALOO_MEDIUM}
-                    />
-                  </TouchableOpacity>
                 </View>
-              </View>
-            </TouchableOpacity>
+              </TouchableOpacity>
+            </BottomSheetView>
           </BottomSheetModal>
         </View>
       </BottomSheetModalProvider>
@@ -240,23 +249,23 @@ const MerchantModal = ({ merchant, setSelectedMerchant }) => {
 const styles = StyleSheet.create({
   container: {},
   contentContainer: {
-    alignItems: "center",
+    alignItems: 'center',
   },
   cardWrapper: {
-    flexDirection: "row",
+    flexDirection: 'row',
     marginHorizontal: 0,
   },
   wrapper: {
     padding: 16,
     borderRadius: 8,
-    ...mainStyles.shadow,
+    paddingBottom: 70,
   },
 
   closeWrapper: {},
   logo: {
     height: 120,
     width: 120,
-    backgroundColor: "grey",
+    backgroundColor: 'grey',
     borderRadius: 8,
   },
   textBlock: {
@@ -265,15 +274,15 @@ const styles = StyleSheet.create({
   },
   name: {
     flexGrow: 1,
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   navigateBtn: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   btn: {
-    flexDirection: "row",
+    flexDirection: 'row',
     borderWidth: 1,
     paddingVertical: 2,
     paddingHorizontal: 6,
